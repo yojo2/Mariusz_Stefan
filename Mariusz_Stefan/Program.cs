919fd0d144e967527daa27dc54ae6dca18467e8d
L-Dogg <szymek94sul@gmail.com>
2017-04-28 22:31:24 +0200

    Add project files.

---
+++ Mariusz_Stefan/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mariusz_Stefan
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
